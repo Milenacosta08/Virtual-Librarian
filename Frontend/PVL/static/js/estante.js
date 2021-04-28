af8d2a8993827308e4cd085f047f08affc6b4498
Milenacosta08 <milena.costa8291@gmail.com>
2021-04-28 17:34:51 -0300

    Atualização do repositório

--- Frontend/PVL/static/js/estante.js
+++ Frontend/PVL/static/js/estante.js
@@ -12,18 +12,4 @@ fileChooser.onchange = e => {
     const reader = new FileReader();
     reader.onload = e => previewImg.src = e.target.result;
     reader.readAsDataURL(fileToUpload);
-};// PRÉVIA DA CAPA DO LIVRO DO MODAL
-const $ = document.querySelector.bind(document);
-
-const previewImg = $('.previa');
-const fileChooser = $('.file-chooser');
-const fileButton = $('.file-button');
-
-fileButton.onclick = () => fileChooser.click();
-
-fileChooser.onchange = e => {
-    const fileToUpload = e.target.files.item(0);
-    const reader = new FileReader();
-    reader.onload = e => previewImg.src = e.target.result;
-    reader.readAsDataURL(fileToUpload);
 };
