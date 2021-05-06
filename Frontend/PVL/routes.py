efbb4b78f3392f721fa88eeaa67a904ad57b4c0c
Milenacosta08 <milena.costa8291@gmail.com>
2021-05-06 19:47:49 -0300

    Atualização das modificações feitas no pythonanywhere

--- Frontend/PVL/routes.py
+++ Frontend/PVL/routes.py
@@ -1,5 +1,9 @@
 from flask import current_app as app, render_template, request
 
+usuarios = [ ]
+acesso = {'logado':0}
+livros = []
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -7,12 +11,39 @@ def home():
 
 @app.route('/feed')
 def feed():
-    return render_template("feed.html")
+    if(acesso['logado']==1):
+        for i in usuarios:
+            nome = i['nome']
+            sobrenome = i['sobrenome']
+        return render_template("feed.html", nome = nome, sobrenome = sobrenome )
+    else:
+        falha = 'Você precisa se cadastrar (ou fazer login) para ter acesso a esta página!'
+        return render_template("index.html", erro = falha)
 
 
 @app.route('/cadastro')
 def cadastro():
     return render_template("cadastro.html")
+
+
+@app.route('/perfil-usuario', methods=['POST'])
+def perfil_log():
+    nome = request.form ['name']
+    sobrenome = request.form ['lastname']
+    email = request.form ['email']
+    senha = request.form ['password']
+    confirma_senha = request.form ['passconfirmation']
+    
+    if (senha == confirma_senha):
+        usuario = {'nome':nome, 'sobrenome':sobrenome, 'email':email, 'senha':senha}
+        usuarios.append(usuario)
+        acesso['logado']=1
+
+        return render_template("perfil.html", nome = usuario['nome'], sobrenome = usuario['sobrenome'], email = usuario['email'])
+    else:
+        erro = 'Senha no campo de confirmação escrita incorretamente!'
+
+        return render_template('cadastro.html', erro = erro)
 
 
 @app.route('/dom-quixote')
@@ -25,6 +56,26 @@ def login():
     return render_template("login.html")
 
 
+@app.route('/feed-usuario', methods=['POST'])
+def feed_log():
+    email = request.form ['email']
+    password = request.form['senha']
+
+    encontrado = 0
+    for usuario in usuarios:
+        if (email == usuario['email'] and password == usuario['senha']):
+            encontrado = 1
+            break
+
+    if(encontrado == 1):
+        acesso['logado']=1
+        return render_template('feed.html', nome = usuario['nome'], sobrenome = usuario['sobrenome'])
+    else:
+        erro = 'Usuário não encontrado'
+
+        return render_template('login.html', erro = erro)
+
+
 @app.route('/pequeno-principe')
 def item2():
     return render_template("item2.html")
@@ -32,7 +83,11 @@ def item2():
 
 @app.route('/perfil')
 def perfil():
-    return render_template("perfil.html")
+    for i in usuarios:
+        nome = i['nome']
+        sobrenome = i['sobrenome']
+        email = i['email']
+    return render_template("perfil.html", nome = nome, sobrenome = sobrenome, email = email)
 
 
 @app.route('/estante')
@@ -42,27 +97,17 @@ def estante():
 
 @app.route('/livro-cadastrado', methods=['POST'])
 def livro_cadastrado():
-    return render_template("estante.html")
+    titulo = request.form["titulo"]
+    if (titulo != ""):
+        livro = {'titulo' : titulo}
+        livros.append(livro)
+
+    return render_template("estante.html", livros = livros[:8], titulo = titulo)
 
 
 @app.route('/cadastrolivros')
 def cadlivros():
     return render_template("cadastrolivros.html")
-
-
-@app.route('/perfil-usuario', methods=['POST'])
-def perfil_log():
-    nome = request.form ['name']
-    sobrenome = request.form ['lastname']
-    email = request.form ['email']
-    senha = request.form ['password']
-    confirma_senha = request.form ['passconfirmation']
-
-    if (senha == confirma_senha):
-        return render_template("perfil.html", nome = nome, sobrenome = sobrenome, email = email, senha = senha)
-    else:
-        erro = 'Errou a senha, trouxa!'
-        return render_template('cadastro.html', erro = erro)
 
 
 @app.route('/buscas')
@@ -78,3 +123,8 @@ def buscas():
             existe = 1
 
     return render_template('buscas.html', busca = buscaz, sites = sites, links = links, existe = existe)
+
+
+@app.route('/amigos')
+def amigo():
+    return render_template('amigos.html')
